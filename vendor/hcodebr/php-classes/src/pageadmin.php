9747d3a787a261374898aea2d901cd388105cd85
D4reD3v1l <white.boy.hell@gmail.com>
2024-02-21 14:16:49 -0400

    criando pageadmin e tpladmin

---
+++ vendor/hcodebr/php-classes/src/pageadmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends PAge {
+
+    public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+}
+
+?>
